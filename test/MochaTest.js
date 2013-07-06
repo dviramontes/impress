a53a8cdfb6c88acc480e0e02569f94ec009efa40
david viramontes <dviramontes@gmail.com>
2013-07-05 22:59:01 -0600

    adds test for index page reques/response

--- test/MochaTest.js
+++ test/MochaTest.js
@@ -16,10 +16,13 @@ describe('Array', function(){
 // also install chai since it allows us to 
 // play arround with diff assertion libraries
 // http://chaijs.com/
+// should vs expect vs assert
 
 var should = require('chai').should();
+
 describe("Array#indexOf()", function(){
 	it("should return -1 when value is not presente", function(){
 		([4,5,6]).indexOf(7).should.equal(-1);
 	})
 })
+
---
+++ test/testIndexPage.js
@@ -0,0 +1,46 @@
+/*
+
+					request the index page 
+					of the app,this is a 
+					local http request.
+					We'll do the authenticated 
+					request next.
+	
+*/
+var should = require('chai').should(),
+	http = require("http");
+
+var options_1 = {
+	hostname : "127.0.0.1",
+	port : "3000",
+	path : "/index",
+	method : "GET"
+}
+
+/*
+
+					request index.html test
+					-----------------------
+
+*/
+
+var requestIndexPage_noAuth = http.request(options_1, function(res){
+
+	describe("Gets index.html from server", function(res){
+		it("should repond with content-type == 'text/html'", function(res){
+			// checks myme-type
+			res.should.be.html;
+		})
+	})
+
+})
+
+requestIndexPage_noAuth.on("error", function(err){
+	if(err) throw err;
+})
+
+requestIndexPage_noAuth.on("data", function(datum){
+	console.log(data);
+})
+
+requestIndexPage_noAuth.end();
