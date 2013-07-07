e95328f07a1533f34b3f2d73328e768a8f5ea1a5
Jesse Nelson <jnels124@msudenver.edu>
2013-07-06 23:28:01 -0600

    Created a data mongo database using mongoose for interaction with the db

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , mongoose = require('mongoose')
   , path = require('path');
 
 var app = express();
@@ -27,9 +28,81 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+mongoose.connect("mongodb://localhost/impress");
+var UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+//Apply schema to database
+Users = mongoose.model('Users', UserSchema);
+
+
+//display list of users....INDEX
+app.get("/users", function(req, res ) {
+  Users.find({}, function(err, docs) { // query for all objects 
+    res.render('users/index', { users: docs });
+  }); 
+});
+
+// New user 
+app.get('/users/new', function(req, res) {
+  res.render("users/new");
+});
+
+// CREATE 
+app.post('/users', function (req, res) {
+  var b = req.body;
+  new Users({
+    name: b.name,
+    email: b.email,
+    password: b.password
+  }).save(function (err, user) {
+    if (err) res.json(err);
+    res.redirect('/users/' + user.name);
+  });
+});
+
+app.param('name', function(req, res, next, name) {
+  Users.find({name: name}, function(err, docs) {
+    req.user = docs[0];
+    next();
+  });
+});
+
+//SHOW
+app.get('/users/:name', function (req, res) {
+  res.render("users/show", { user: req.user } );
+});
+
+//update
+app.put('/users/:name', function(req,res) {
+  var b = req.body;
+  Users.update(
+    { name: req.params.name},
+    { name: b.name, password: b.password, email: b.email },
+    function ( err ) {
+      res.redirect("/users/" + b.name);
+    });
+});
+
+//Edit
+app.get('/users/:name/edit', function (req, res) {
+  res.render("users/edit", { user: req.user });
+});
+
+
+// Delete
+app.delete('/users/:name', function ( req, res ) {
+  Users.remove({ name: req.params.name}, function (err) {
+    res.redirect('/users/');
+  });
+});
+
